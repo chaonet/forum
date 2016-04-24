5f62890e007e74a9cc094d106122e1e2ef1881fc
chaonet <xuchaorfc@gmail.com>
2016-04-24 23:40:08 +0800

    del some desc

--- myforum/article/views.py
+++ myforum/article/views.py
@@ -9,7 +9,5 @@ from models import Article
 def article_list(request,block_id):
 	block_id = int(block_id)
 	block = Block.objects.get(id=block_id)
-	# print block
-	# 部落建设  版块名称
 	articles = Article.objects.filter(block=block).order_by("-last_update_timestamp")
 	return render(request, "article_list.html", {"articles":articles,"block":block})
