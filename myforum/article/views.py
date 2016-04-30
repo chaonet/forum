56755e8dd7ad16263a5a4d36ad084d112397e7f5
chaonet <xuchaorfc@gmail.com>
2016-04-30 23:40:54 +0800

    add messages

--- myforum/article/views.py
+++ myforum/article/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
+from django.contrib import messages
 
 # Create your views here.
 from django.contrib.auth.models import User
@@ -25,12 +26,14 @@ def article_create(request,block_id):
         title = request.POST["title"].strip()
         content = request.POST["content"].strip()
         if not request.POST["title"] or not request.POST["content"]:
+            messages.add_message(request, messages.INFO, u'标题和内容不能空！')
             return render(request, "article_create.html", {"b":block,'title':title,'article_content':content})
         owner = User.objects.all()[0] # TODO:
         request.POST["title"].strip()  # request 对象，字典
         request.POST["content"].strip()
         new_article = Article(block=block,owner=owner,title=title,content=content)
         new_article.save()
+        messages.add_message(request, messages.INFO, u'发表成功！')
         return redirect(reverse('article_list',args=[block.id]))
 
 def article_detail(request,article_id):
