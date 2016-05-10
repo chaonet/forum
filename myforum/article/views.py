90cd91a80c446979c76ecabf722e69a8d3db063e
chaonet <xuchaorfc@gmail.com>
2016-05-10 09:57:02 +0800

    add paginator

--- myforum/article/views.py
+++ myforum/article/views.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 from block.models import Block
 from models import Article
@@ -13,7 +14,32 @@ def article_list(request,block_id):
     block_id = int(block_id)
     block = Block.objects.get(id=block_id)
     articles = Article.objects.filter(block=block).order_by("-last_update_timestamp")
-    return render(request, "article_list.html", {"articles":articles,"b":block})
+    # return render(request, "article_list.html", {"articles":articles,"b":block})
+
+    page_no = int(request.GET.get('page_no','1'))
+    p = Paginator(articles, 1)
+    if page_no < 0:
+        page_no = 1
+    if page_no > p.num_pages:
+        page_no = p.num_pages
+    page_list = [i for i in range(page_no - 5, page_no + 6) if i > 0 and i <= p.num_pages]
+    page = p.page(page_no)
+    previous_link = page_list[0] - 1
+    next_link = page_list[-1] + 1
+    first_link = page_list[0] - 2
+    last_link = page_list[-1] + 2
+    return render(request, 'article_list.html', 
+                  {"articles":page.object_list,"b":block,
+                   "has_previous":previous_link>0, "has_next":next_link<=p.num_pages,
+                   "has_first":first_link>0, "has_last":last_link<=p.num_pages,
+                   "previous_link":previous_link,
+                   "next_link":next_link,
+                   "current_no":page_no,
+                   "pages_num":p.num_pages,
+                   "page_list":page_list
+                  }
+                 )
+    
 
 @login_required
 def article_create(request,block_id):
