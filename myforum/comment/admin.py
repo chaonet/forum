a4ee228aec0b87508d58da92541df4042ce10235
chaonet <xuchaorfc@gmail.com>
2016-05-13 22:38:55 +0800

    add commit app and models

---
+++ myforum/comment/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from models import Comment
+
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ("block", "article", "owner", 
+					"content", "status", "to_comment_id", 
+					"create_timestamp", "last_update_timestamp"
+				   )
+	list_filter = ("block",)
+
+admin.site.register(Comment, CommentAdmin)
--- myforum/myforum/settings.py
+++ myforum/myforum/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'block',
     'article',
     'usercenter',
+    'comment',
 ]
 
 MIDDLEWARE_CLASSES = [
