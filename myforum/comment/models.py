686de8b8db9554eba4cd712fe8468ff6c356b9b4
chaonet <xuchaorfc@gmail.com>
2016-07-03 10:34:08 +0800

    comment to comment

--- myforum/comment/models.py
+++ myforum/comment/models.py
@@ -19,6 +19,13 @@ class Comment(models.Model):
 	create_timestamp = models.DateTimeField(u"创建时间", auto_now_add=True)
 	last_update_timestamp = models.DateTimeField(u"最后更新时间", auto_now=True)
 
+	@property
+	def to_comment(self):
+		if not self.to_comment_id:
+			return None
+		else:
+			return Comment.objects.get(id=self.to_comment_id)
+
 	def __unicode__(self):
 		return self.content[:20]
 
