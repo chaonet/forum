d9cc2226caed6d2e5ca7ffd95d226a9cef67854a
chaonet <xuchaorfc@gmail.com>
2016-07-09 12:05:03 +0800

    Django admin site

--- myforum/article/admin.py
+++ myforum/article/admin.py
@@ -3,12 +3,29 @@
 from django.contrib import admin
 
 from models import Article
+from comment.models import Comment
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    can_delete = False
+    raw_id_fields = ("owner",)
+    readonly_fields = ("block", "to_comment_id", "owner", "content", "status")
+
+    # 作用同 can_delete = False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
+
+    def has_add_permission(self, request):
+        return False
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ("block", "owner", "title", "content", "status","create_timestamp","last_update_timestamp")
     search_fields = ["title","content"]
     list_filter = ("block", )
     actions = ['make_published']
+    inlines = [
+        CommentInline,
+    ]
 
     def make_published(self, request, queryset):
         rows_updated = queryset.update(status=1)
